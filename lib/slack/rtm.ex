ad41638b1aab9928b1bc77082dde4bce45a2f9ad
Kevin Smith <kevin@operable.io>
2016-10-06 10:28:32 -0400

    Log when Slack returns non-JSON

--- lib/slack/rtm.ex
+++ lib/slack/rtm.ex
@@ -2,11 +2,21 @@ defmodule Slack.Rtm do
   @moduledoc false
   @url "https://slack.com/api/rtm.start?token="
 
+  require Logger
+
   def start(token) do
     case HTTPoison.get(@url <> token) do
       {:ok, response} ->
-        json = JSX.decode!(response.body, [{:labels, :atom}])
-        {:ok, json}
+        case JSX.decode(response.body, [{:labels, :atom}]) do
+          {:ok, json} ->
+            {:ok, json}
+          error ->
+            Logger.error("""
+            Slack API returned non-JSON output: #{inspect response.body, pretty: true}
+            HTTPoison returned: #{inspect error, pretty: true}
+            """)
+            raise RuntimeError, message: "Slack API returned non-JSON output"
+        end
       {:error, reason} -> {:error, reason}
     end
   end
